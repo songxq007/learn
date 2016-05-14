ccd4bae30882ce8eae099a0ef1e722e4922af435
grace <751026660@qq.com>
2016-05-14 11:59:56 +0800

    jenkins02

---
+++ song/src/test/java/cn/hfax1/song/Test02.java
@@ -0,0 +1,15 @@
+package cn.hfax1.song;
+
+import junit.framework.TestCase;
+
+public class Test02 extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
